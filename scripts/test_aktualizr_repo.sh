ef1bd4f866d103be1cdfd8614674fb164a3368bf
Laurent Bonnans <laurent.bonnans@here.com>
2019-06-03 14:23:18 +0200

    Streamline gitlab CI config
    - split some stages
    - do not run docker commands in test/build stages
    
    Signed-off-by: Laurent Bonnans <laurent.bonnans@here.com>

--- scripts/test_aktualizr_repo.sh
+++ scripts/test_aktualizr_repo.sh
@@ -7,11 +7,11 @@ dpkg-deb -I /persistent/aktualizr*.deb && dpkg -i /persistent/aktualizr*.deb
 export PATH=${PATH}:/persistent
 
 TMPDIR=$(mktemp -u)
-create_repo.sh $TMPDIR 127.0.0.1
+create_repo.sh "$TMPDIR" 127.0.0.1
 serve_repo.py 9000 "$TMPDIR" &
 
 aktualizr --config "${TMPDIR}/sota.toml" --run-mode once
 
 aktualizr-info --config "${TMPDIR}/sota.toml" | grep "Fetched metadata: yes" || exit 1
 
-rm -r $TMPDIR
+rm -r "$TMPDIR"
