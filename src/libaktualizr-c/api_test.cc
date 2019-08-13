ec3c150e3be1c970a07895ad232d7b99967a4e2b
Eugene Smirnov <evgenii.smirnov@here.com>
2019-08-13 16:22:28 +0200

    wip

--- src/libaktualizr-c/api_test.cc
+++ src/libaktualizr-c/api_test.cc
@@ -1,6 +1,5 @@
 #include <gtest/gtest.h>
 
-#include <future>
 #include <iostream>
 #include <string>
 #include <random>
@@ -12,21 +11,13 @@
 
 #include "config/config.h"
 #include "logging/logging.h"
-#include "package_manager/ostreemanager.h"
-#include "primary/aktualizr.h"
-#include "storage/sqlstorage.h"
 #include "test_utils.h"
 
 boost::filesystem::path aktualizr_repo_path;
 static std::string server = "http://127.0.0.1:";
 static boost::filesystem::path sysroot;
 
-static std::string new_rev;
-
-/*
- * Install an OSTree update on the primary.
- */
-TEST(Aktualizr, FullOstreeUpdate) {
+TEST(Aktualizr, ApiTest) {
   TemporaryDirectory temp_dir;
   Config conf = UptaneTestCommon::makeTestConfig(temp_dir, server);
   conf.pacman.type = PackageManager::kOstree;
@@ -39,39 +30,6 @@ TEST(Aktualizr, FullOstreeUpdate) {
 
   LOG_INFO << "conf: " << conf;
 
-  {
-    Aktualizr aktualizr(conf);
-
-    aktualizr.Initialize();
-
-    result::UpdateCheck update_result = aktualizr.CheckUpdates().get();
-    ASSERT_EQ(update_result.status, result::UpdateStatus::kUpdatesAvailable);
-
-    result::Download download_result = aktualizr.Download(update_result.updates).get();
-    EXPECT_EQ(download_result.status, result::DownloadStatus::kSuccess);
-
-    result::Install install_result = aktualizr.Install(update_result.updates).get();
-    EXPECT_EQ(install_result.ecu_reports.size(), 1);
-    EXPECT_EQ(install_result.ecu_reports[0].install_res.result_code.num_code,
-              data::ResultCode::Numeric::kNeedCompletion);
-  }
-
-  // do "reboot" and finalize
-  ostree_deployment.serial = 1;
-  ostree_deployment.rev = new_rev;
-  boost::filesystem::remove(conf.bootloader.reboot_sentinel_dir / conf.bootloader.reboot_sentinel_name);
-
-  {
-    UptaneTestCommon::TestAktualizr aktualizr(conf);
-    aktualizr.Initialize();
-
-    result::UpdateCheck update_result = aktualizr.CheckUpdates().get();
-    ASSERT_EQ(update_result.status, result::UpdateStatus::kNoUpdatesAvailable);
-
-    // check new version
-    const auto target = aktualizr.uptane_client()->package_manager_->getCurrent();
-    EXPECT_EQ(target.sha256Hash(), new_rev);
-  }
 }
 
 #ifndef __NO_MAIN__
